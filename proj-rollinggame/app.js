5ca63f7955132e1a8d58baa3e79e0ae70d5965c6
richard <richardlyk1108@gmail.com>
2019-08-13 01:16:33 +0800

    add double 6

--- proj-rollinggame/app.js
+++ proj-rollinggame/app.js
@@ -12,47 +12,49 @@ GAME RULES:
 
 
 
-var score ,roundScore ,activePlayer ;
+var score ,roundScore ,activePlayer ,gamePlaying,previous_dice,dice_1,dice_2;
 
+init();
 
-
-score=[0,0];
-roundScore=0;
-activePlayer=0;
-
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
 
 
 document.querySelector('.btn-roll').addEventListener('click',function(){
 	//Do something here
-	var dice=Math.ceil(Math.random()*6);
+	if(gamePlaying){
 
+
+	var dice=Math.ceil(Math.random()*6);
+	if(previous_dice===6 & dice===6){
+		document.querySelector('#score-'+activePlayer).textContent =0;
+	}
 	var diceDOM = document.querySelector(".dice");
 	diceDOM.style.display = 'block';
 	diceDOM.src = 'dice-'+dice +'.png';
 
+	previous_dice = dice;
 	if(dice !== 1){
 		roundScore+=dice;
 		document.querySelector('#current-'+activePlayer).textContent = roundScore;
 	}else{
+		if(activePlayer===1){
+			dice_1 = previous_dice;
+		}
+		else{
+			dice_2 = previous_dice;
+		}
 		nextPlayer();
 
 
 	}
 
-
+}
 
 });
 
 
 document.querySelector('.btn-hold').addEventListener('click',function(){
+	if(gamePlaying){
 	score[activePlayer] += roundScore;
 	document.querySelector('#score-'+activePlayer).textContent = score[activePlayer];
 	
@@ -61,12 +63,12 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
 		document.querySelector('.dice').style.display = 'none';
 		document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner')
 		document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active')
-
+		gamePlaying = false;
 	}else{
 
 	nextPlayer();	
 	}
-	
+	}
 
 
 });
@@ -84,3 +86,26 @@ function nextPlayer(){
 		
 
 };
+
+document.querySelector('.btn-new').addEventListener('click',init);
+
+function init(){
+	score = [ 0 ,0 ];
+	activePlayer = 0 ;
+	roundScore = 0;	
+	gamePlaying = true;
+	previous_dice =0
+	document.querySelector('.dice').style.display = 'none';
+
+	document.getElementById('score-0').textContent = '0';
+	document.getElementById('score-1').textContent = '0';
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+	document.getElementById("name-0").textContent = 'Player 1';
+	document.getElementById("name-1").textContent = 'Player 2';
+	document.querySelector('.player-0-panel').classList.remove('winner')
+	document.querySelector('.player-1-panel').classList.remove('winner')
+	document.querySelector('.player-0-panel').classList.remove('active')
+	document.querySelector('.player-1-panel').classList.remove('active')
+	document.querySelector('.player-0-panel').classList.add('active')
+}
