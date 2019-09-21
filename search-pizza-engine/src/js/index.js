ea481a03e847c4d17bb0c445e9edefdc33e083e3
richard <richardlyk1108@gmail.com>
2019-09-21 21:46:56 +0800

    get pizza data from 3rd party api

--- search-pizza-engine/src/js/index.js
+++ search-pizza-engine/src/js/index.js
@@ -1,1 +1,23 @@
 // Global app controller
+
+
+//https://www.food2fork.com/api/search
+//374ee35083b9d6960df0e9a1e7af04db
+
+import axios from 'axios';
+
+async function getResults(query){
+    const proxy = 'https://crossorigin.me/';
+    const key = "374ee35083b9d6960df0e9a1e7af04db";
+
+    try{
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes
+        console.log(recipes);
+    }catch(error){
+        alert(error);
+    }
+
+}
+
+getResults('pizza');
